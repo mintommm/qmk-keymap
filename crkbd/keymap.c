a905ffff85de1882037fe860228611fd9d245a6b
Terry K <1401820+mintommm@users.noreply.github.com>
2024-03-31 12:33:47 +0900

    Update keymap.c

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -3,7 +3,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
-Copyright 2024 @mintommm
+Copyright 2020 @mintommm
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_E,    KC_I,    KC_A,    KC_O, KC_MINS,                         KC_K,    KC_T,    KC_N,    KC_S,    KC_H, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_V, KC_SCLN,                         KC_G,    KC_D,    KC_M,    KC_J,    KC_B, KC_BSLS,
+      KC_LALT,    KC_Z,    KC_X,    KC_C,    KC_V, KC_SCLN,                         KC_G,    KC_D,    KC_M,    KC_J,    KC_B, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LCTL,   LOWER,LSFT_SPC,   RSFT_ENT,   RAISE, KC_BSPC
                                       //`--------------------------'  `--------------------------'
@@ -51,11 +51,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_LGUI, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_GRV,
+      KC_LGUI, XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_BSPC,                      KC_LPRN, KC_RPRN, KC_ASTR, KC_PIPE, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   LCTL(KC_W), KC_QUES, KC_HOME,   KC_UP,  KC_END, KC_BSPC,                       KC_EQL, KC_MINS, KC_PPLS, KC_LBRC, KC_RBRC, KC_BSLS,
+   LCTL(KC_W), KC_QUES, KC_LEFT, KC_DOWN,  KC_RGHT, KC_ENT,                      KC_LBRC, KC_RBRC,  KC_EQL, KC_PPLS, KC_PMNS, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT,                      KC_DQUO, KC_QUOT, KC_SLSH,   KC_LT,   KC_GT, KC_UNDS,
+      KC_LALT, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                        KC_LT,   KC_GT, KC_CIRC, KC_AMPR, KC_UNDS, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -63,11 +63,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_LGUI,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
+      KC_LGUI,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_PSCR,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F11, KC_HOME,   KC_UP,  KC_END, KC_BSPC, XXXXXXX,
+      KC_PSCR,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LALT,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                       KC_F12, KC_LEFT, KC_DOWN, KC_RGHT,  KC_ENT, XXXXXXX,
+      KC_LALT,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,                      KC_HOME, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_END,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -77,9 +77,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX,   KC_P0,   KC_P7,   KC_P8,   KC_P9, KC_PDOT,                      XXXXXXX, XXXXXXX, KC_PAST, XXXXXXX, XXXXXXX, LGUI(KC_L),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_P0,   KC_P4,   KC_P5,   KC_P6, KC_COMM,                       KC_EQL, KC_PMNS, KC_PPLS, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_P0,   KC_P4,   KC_P5,   KC_P6, KC_COMM,                      XXXXXXX, XXXXXXX, KC_EQL,  KC_PPLS, KC_PMNS, KC_PSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_P0,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, KC_PSLS, XXXXXXX, XXXXXXX, XXXXXXX,
+      XXXXXXX,   KC_P0,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
