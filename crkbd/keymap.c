788ff9cf5b476b7293f7d1b59d7396038270c0cb
Terry K <1401820+mintommm@users.noreply.github.com>
2024-05-31 10:27:32 +0900

    Update keymap.c

--- crkbd/keymap.c
+++ crkbd/keymap.c
@@ -3,7 +3,7 @@
 /*
 Copyright 2019 @foostan
 Copyright 2020 Drashna Jaelre <@drashna>
-Copyright 2020 @mintommm
+Copyright 2024 @mintommm
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -38,7 +38,7 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_L,    KC_U, KC_COMM,  KC_DOT,                         KC_F,    KC_W,    KC_R,    KC_Y,    KC_P,  KC_DEL,
+       KC_ESC,    KC_Q,    KC_L,    KC_U, KC_COMM,  KC_DOT,                         KC_F,    KC_W,    KC_R,    KC_Y,    KC_P,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_TAB,    KC_E,    KC_I,    KC_A,    KC_O, KC_MINS,                         KC_K,    KC_T,    KC_N,    KC_S,    KC_H, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -46,16 +46,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LALT,   LOWER,LSFT_SPC,   RSFT_ENT,   RAISE, KC_BSPC
                                       //`--------------------------'  `--------------------------'
-
   ),
 
     [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_LGUI, XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_BSPC,                      KC_LPRN, KC_RPRN, KC_ASTR, KC_PIPE, KC_BSLS,  KC_GRV,
+   LCTL(KC_W),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_LBRC, KC_RBRC,  KC_EQL, KC_PIPE, KC_BSLS,  KC_GRV,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   LCTL(KC_W), KC_QUES, KC_LEFT, KC_DOWN,  KC_RGHT, KC_ENT,                      KC_LBRC, KC_RBRC,  KC_EQL, KC_PPLS, KC_PMNS, KC_SLSH,
+      KC_BSPC,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_LPRN, KC_RPRN, KC_PPLS, KC_PMNS, KC_ASTR, KC_SLSH,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                        KC_LT,   KC_GT, KC_CIRC, KC_AMPR, KC_UNDS, KC_QUOT,
+      _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_ENT,                        KC_LT,   KC_GT, KC_CIRC, KC_AMPR, KC_UNDS, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -63,11 +62,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      KC_LGUI,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                         KC_1,    KC_2,    KC_3,    KC_4,    KC_5, XXXXXXX,
+      KC_LGUI,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_PSCR,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
+      KC_PSCR,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                      KC_QUES, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,  KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,                      KC_HOME, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_END,
+      _______,  KC_F11,  KC_F12, XXXXXXX, XXXXXXX, XXXXXXX,                      KC_SPC,  KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
@@ -75,18 +74,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_ADJUST] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX,   KC_P0,   KC_P7,   KC_P8,   KC_P9, KC_PDOT,                      XXXXXXX, XXXXXXX, KC_PAST, XXXXXXX, XXXXXXX, LGUI(KC_L),
+      XXXXXXX,   KC_P0,   KC_P7,   KC_P8,   KC_P9, KC_PDOT,                      XXXXXXX, XXXXXXX, KC_EQL,  XXXXXXX, XXXXXXX, LGUI(KC_L),
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_P0,   KC_P4,   KC_P5,   KC_P6, KC_COMM,                      XXXXXXX, XXXXXXX, KC_EQL,  KC_PPLS, KC_PMNS, KC_PSLS,
+      XXXXXXX,   KC_P0,   KC_P4,   KC_P5,   KC_P6, KC_COMM,                      XXXXXXX, XXXXXXX, KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX,   KC_P0,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      _______,   KC_P0,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
 
-// https://okapies.hateblo.jp/entry/2019/02/02/133953
 static bool lower_pressed = false;
 static uint16_t lower_pressed_time = 0;
 static bool raise_pressed = false;
@@ -110,20 +108,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         update_tri_layer(_LOWER, _RAISE, _ADJUST);
 
         if (lower_pressed && (TIMER_DIFF_16(record->event.time, lower_pressed_time) < TAPPING_TERM)) {
-        // https://note.com/vast_moraea865/n/n1f2d8d736059
-        // https://github.com/qmk/qmk_firmware/pull/14726
-        // https://zenn.dev/duck_falcon/articles/3520a609e84ed6
-        // https://nest.pijul.com/sunflower/qmk_firmware:main/BYRPMFHCIOX2Y.GV7ICBA
           tap_code(KC_INTERNATIONAL_5);
           tap_code(KC_LANGUAGE_2);
-        //   register_code(KC_INTERNATIONAL_5);
-        //   unregister_code(KC_INTERNATIONAL_5);
-        //   register_code(KC_LANGUAGE_2); // for macOS
-        //   unregister_code(KC_LANGUAGE_2);
-        //   register_code(JP_MHEN);
-        //   unregister_code(JP_MHEN);
-        //   register_code(KR_HANJ);
-        //   unregister_code(KR_HANJ);
         }
         lower_pressed = false;
       }
@@ -146,17 +132,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           tap_code(KC_LANGUAGE_1);
 
           // chromeOS用、LCTLだと物理キーで使ってるLCTLと競合してここでUnregisterされる可能性があるのでRCTLを使う
+          // 副作用としてWindowsで半角スペースが入力される。Macはわからない
           register_code(KC_RCTL);
           tap_code(KC_SPC);
           unregister_code(KC_RCTL);
-        //   register_code(KC_INTERNATIONAL_4);
-        //   unregister_code(KC_INTERNATIONAL_4);
-        //   register_code(KC_LANGUAGE_1); // for macOS
-        //   unregister_code(KC_LANGUAGE_1);
-        //   register_code(JP_HENK);
-        //   unregister_code(JP_HENK);
-        //   register_code(KR_HAEN);
-        //   unregister_code(KR_HAEN);
         }
         raise_pressed = false;
       }
@@ -204,7 +183,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     default:
       if (record->event.pressed) {
-        // reset the flags
         lower_pressed = false;
         raise_pressed = false;
         space_pressed = false;
